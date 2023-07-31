9f340af42d563040fbf047be72a79e6678815d83
Asilvorcarp <asilvorcarp@qq.com>
2023-08-01 02:07:45 +0800

    chore

--- plugins/always_focus.sh
+++ plugins/always_focus.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 FOCUS="/Users/asc/repo/_cloned_/focus/bin/focus"
-CMD_START_SESSION='tell application "Keyboard Maestro Engine" to do script "Start a session"'
+CMD_START_SESSION='tell application "Keyboard Maestro Engine" to do script "Ask to start a session"'
 
 if [[ `pgrep focus` == "" ]]; then
     echo start a session
     nohup osascript -e "$CMD_START_SESSION" > /dev/null 2>&1 &
     exit
 fi
+
+# note:
+# - set
+# osascript -e 'tell application "Keyboard Maestro Engine" to setvariable "My KM Var" to "Some new value"'
+# - get
+# osascript -e 'tell application "Keyboard Maestro Engine" to getvariable "My KM Var"'
