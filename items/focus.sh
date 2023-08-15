3db6e04ce4b3090e317ceda7e145a3af65f30e0f
Asilvorcarp <asilvorcarp@qq.com>
2023-08-15 20:07:05 +0800

    Add locking state and fix some bug

--- items/focus.sh
+++ items/focus.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 focus=(
-  icon=cal
-  icon.font="$FONT:Black:12.0"
-  icon.padding_right=6
-  label.font="$FONT:Black:12.0"
-  padding_right=16
-  label.width=45
-  update_freq=1
-  script="$PLUGIN_DIR/focus.sh"
-#   label.align=right
-#   padding_left=15
+	icon=cal
+	icon.font="$FONT:Black:12.0"
+	icon.padding_right=6
+	label.font="$FONT:Black:12.0"
+	padding_right=16
+	label.width=45
+	update_freq=1
+	script="$PLUGIN_DIR/focus.sh"
+	#   label.align=right
+	#   padding_left=15
 )
 
 # keep on reminding to start a focus session
 always_focus=(
-  width=0
-  label.width=0
-  drawing=off
-  label.drawing=off
-  update_freq=180
-  script="$PLUGIN_DIR/always_focus.sh"
+	# width=0
+	# label.width=0
+	# drawing=off
+	# label.drawing=off
+	update_freq=180
+	script="$PLUGIN_DIR/always_focus.sh"
 )
 
-sketchybar --add item focus right           \
-           --set focus "${focus[@]}"        \
-           --subscribe focus mouse.clicked  \
-                             mouse.entered  \
-                             mouse.exited   \
-           --add item always_focus right    \
-           --set always_focus "${always_focus[@]}"  \
+sketchybar --add item focus right \
+	--set focus "${focus[@]}" \
+	--subscribe focus mouse.clicked \
+	mouse.entered \
+	mouse.exited \
+	--add item always_focus right \
+	--set always_focus "${always_focus[@]}"
--- plugins/focus.sh
+++ plugins/focus.sh
@@ -4,78 +4,93 @@ source "$CONFIG_DIR/colors.sh"
 source "$CONFIG_DIR/icons.sh"
 FOCUS="/Users/asc/repo/_cloned_/focus/bin/focus"
 
-line=`$FOCUS status | grep "\["`
+line=$($FOCUS status | grep "\[")
 
-state=`echo $line | awk '{print $1 " " $2}'`
+state=$(echo $line | awk '{print $1 " " $2}')
 # [Work 2/4]: or [Short break]: or [Not Running]
 if [[ $state == *"break"* ]]; then
-    ic="$RESTING"
+	ic="$RESTING"
 else
-    ic="$WORKING"
+	ic="$WORKING"
 fi
 
-t=`echo $line | awk '{print $3}'`
+t=$(echo $line | awk '{print $3}')
 la="$t"
 
 if [[ $state == *"Not Running"* ]]; then
-    ic="$WORKING"
-    la="Start"
+	ic="$WORKING"
+	la="Start"
+fi
+
+# change icon when mutex is on
+# i.e. no prompt to ask for starting a session in idle time
+mutex=$(osascript -e 'tell application "Keyboard Maestro Engine" to getvariable "promptMutex"')
+if [[ $mutex == "on" ]]; then
+	ic="$LOCKING"
 fi
 
 # update
-
 sketchybar --set focus icon="$ic" label="$la"
-
 
 # handle click
 
 left_click() {
-    echo left!
-    if [[ $state == *"Not Running"* ]]; then
-        $FOCUS resume
-    else
-        killall -INT focus
-    fi
+	echo left!
+	if [ "$MODIFIER" = "cmd" ]; then
+		if [[ $mutex == "off" ]]; then
+			osascript -e 'tell application "Keyboard Maestro Engine" to setvariable "promptMutex" to "on"'
+		else
+			osascript -e 'tell application "Keyboard Maestro Engine" to setvariable "promptMutex" to "off"'
+		fi
+	else
+		if [[ $state == *"Not Running"* ]]; then
+			$FOCUS resume
+		else
+			killall -INT focus
+		fi
+	fi
 }
 
 right_click() {
-    echo right!
-    if [[ $state == *"Not Running"* ]]; then
-        if [ "$MODIFIER" = "cmd" ]; then
-            $FOCUS -w 0 -s 0
-        else
-            $FOCUS
-        fi
-    else
-        killall -INT focus
-    fi
+	echo right!
+	if [[ $state == *"Not Running"* ]]; then
+		if [ "$MODIFIER" = "cmd" ]; then
+			$FOCUS -w 0 -s 0
+		else
+			$FOCUS
+		fi
+	else
+		killall -INT focus
+	fi
 }
 
 mouse_clicked() {
-    case "$BUTTON" in
-        "left") left_click
-        ;;
-        "right") right_click
-        ;;
-    esac
+	case "$BUTTON" in
+	"left")
+		left_click
+		;;
+	"right")
+		right_click
+		;;
+	esac
 }
 
-
-
 mouse_entered() {
-    sketchybar --set focus icon.color=$BLUE label.color=$BLUE
+	sketchybar --set focus icon.color=$BLUE label.color=$BLUE
 }
 
 mouse_exited() {
-    sketchybar --set focus icon.color=$ICON_COLOR label.color=$LABEL_COLOR
+	sketchybar --set focus icon.color=$ICON_COLOR label.color=$LABEL_COLOR
 }
 
-
 case "$SENDER" in
-    "mouse.clicked") mouse_clicked
-    ;;
-    "mouse.entered") mouse_entered
-    ;;
-    "mouse.exited") mouse_exited
-    ;;
+"mouse.clicked")
+	mouse_clicked
+	;;
+"mouse.entered")
+	mouse_entered
+	;;
+"mouse.exited")
+	mouse_exited
+	;;
 esac
